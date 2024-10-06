998046189cb9cefbccebb6c4b7fac99fb1722301
Tomas-L-Apal <tapaldetti@fi.uba.ar>
2024-10-05 21:02:56 -0300

    storage

---
+++ common/serialize.go
@@ -0,0 +1,108 @@
+package common
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+type Serializable interface {
+	Serialize() []byte
+}
+
+type Deserialize[T any] func([]byte) (T, error)
+
+type Serializer struct {
+	buf bytes.Buffer
+}
+
+func NewSerializer() Serializer {
+	var buf bytes.Buffer
+	return Serializer{
+		buf,
+	}
+}
+
+func (s *Serializer) WriteString(str string) *Serializer {
+	s.WriteUint32((uint32(len(str))))
+	s.buf.WriteString(str)
+	return s
+}
+
+func (s *Serializer) WriteInt32(n int32) *Serializer {
+	binary.Write(&s.buf, binary.LittleEndian, n)
+	return s
+}
+
+func (s *Serializer) WriteUint32(n uint32) *Serializer {
+	binary.Write(&s.buf, binary.LittleEndian, n)
+	return s
+}
+
+func (s *Serializer) WriteUint8(n uint8) *Serializer {
+	binary.Write(&s.buf, binary.LittleEndian, n)
+	return s
+}
+
+func (s *Serializer) WriteBytes(b []byte) *Serializer {
+	s.buf.Write(b)
+	return s
+}
+
+func (s *Serializer) ToBytes() []byte {
+	return s.buf.Bytes()
+}
+
+func (s *Serializer) WriteArray(serializables []Serializable) *Serializer {
+	l := uint32(len(serializables))
+	s.WriteUint32(l)
+	for _, ser := range serializables {
+		s.WriteBytes(ser.Serialize())
+	}
+	return s
+}
+
+type Deserializer struct {
+	buf *bytes.Buffer
+}
+
+func NewDeserializer(data []byte) Deserializer {
+	return Deserializer{
+		buf: bytes.NewBuffer(data),
+	}
+}
+
+func (d *Deserializer) ReadString() (string, error) {
+	length, err := d.ReadUint32()
+	if err != nil {
+		return "", err
+	}
+	str := make([]byte, length)
+	if _, err := d.buf.Read(str); err != nil {
+		return "", err
+	}
+	return string(str), nil
+}
+
+func (d *Deserializer) ReadUint32() (uint32, error) {
+	var n uint32
+	if err := binary.Read(d.buf, binary.LittleEndian, &n); err != nil {
+		return 0, err
+	}
+	return n, nil
+}
+
+func (d *Deserializer) ReadInt32() (int32, error) {
+	var n int32
+	if err := binary.Read(d.buf, binary.LittleEndian, &n); err != nil {
+		return 0, err
+	}
+	return n, nil
+}
+
+func (d *Deserializer) ReadUint8() (uint8, error) {
+	var n uint8
+	if err := binary.Read(d.buf, binary.LittleEndian, &n); err != nil {
+		return 0, err
+	}
+	return n, nil
+}
---
+++ common/storage.go
@@ -0,0 +1,133 @@
+package common
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/op/go-logging"
+)
+
+var log = logging.MustGetLogger("log")
+
+type TemporaryStorage struct {
+	filepath string
+	file     *os.File
+	term     chan os.Signal
+}
+
+type ClosedFileError struct {
+	filepath string
+}
+
+func (m *ClosedFileError) Error() string {
+	return fmt.Sprintf("The file %s was already closed. This can be due to manual Close() call or due to a SYSCALL received", m.filepath)
+}
+
+func NewTemporaryStorage(path string, cache bool) (*TemporaryStorage, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	t := &TemporaryStorage{
+		filepath: path,
+		file:     f,
+		term:     make(chan os.Signal, 1),
+	}
+
+	signal.Notify(t.term, syscall.SIGTERM)
+	go t.handleShutdown()
+	return t, nil
+}
+
+func (t *TemporaryStorage) Close() {
+	if t.file == nil {
+		return
+	}
+
+	err := t.file.Close()
+	if err != nil {
+		log.Errorf("Failed to close correctly the file %s: %s", t.filepath, err)
+	}
+	t.file = nil
+}
+
+func (t *TemporaryStorage) handleShutdown() {
+	<-t.term
+	t.Close()
+}
+
+func (t *TemporaryStorage) Overwrite(data []byte) (int, error) {
+	if t.file == nil {
+		return -1, &ClosedFileError{
+			filepath: t.filepath,
+		}
+	}
+	t.file.Truncate(0)
+	t.Reset()
+	return t.file.Write(data)
+}
+
+func (t *TemporaryStorage) SaveState(state Serializable) (int, error) {
+	return t.Overwrite(state.Serialize())
+}
+
+func (t *TemporaryStorage) Append(data []byte) (int, error) {
+	if t.file == nil {
+		return -1, &ClosedFileError{
+			filepath: t.filepath,
+		}
+	}
+	return t.file.Write(data)
+}
+
+func (t *TemporaryStorage) Reset() {
+	if t.file == nil {
+		return
+	}
+	t.file.Seek(0, io.SeekStart)
+}
+
+func (t *TemporaryStorage) ReadAll() ([]byte, error) {
+	t.Reset()
+	if t.file == nil {
+		return nil, &ClosedFileError{
+			filepath: t.filepath,
+		}
+	}
+
+	b, err := io.ReadAll(t.file)
+	return b, err
+}
+
+func LoadState[T any](t *TemporaryStorage, d Deserialize[T]) (T, error) {
+	ds, err := t.ReadAll()
+	if err != nil {
+		var zeroValue T
+		return zeroValue, err
+	}
+	return d(ds)
+}
+
+func (t *TemporaryStorage) File() (*os.File, error) {
+	if t.file == nil {
+		return nil, &ClosedFileError{
+			filepath: t.filepath,
+		}
+	}
+
+	return t.file, nil
+}
+
+func (t *TemporaryStorage) Scanner() (*bufio.Scanner, error) {
+	if t.file == nil {
+		return nil, &ClosedFileError{
+			filepath: t.filepath,
+		}
+	}
+
+	return bufio.NewScanner(t.file), nil
+}
