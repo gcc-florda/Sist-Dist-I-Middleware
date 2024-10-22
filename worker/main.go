aea7cc2d25717b3fade73878fb020d4f4944f463
Tomas-L-Apal <tapaldetti@fi.uba.ar>
2024-10-22 16:22:00 -0300

    worker init

--- worker/main.go
+++ worker/main.go
@@ -3,110 +3,61 @@ package main
 import (
 	"middleware/common"
 	"middleware/rabbitmq"
-	"middleware/worker/business"
 	"middleware/worker/controller"
+	"sync"
 
 	"github.com/op/go-logging"
 )
 
+type ControllerFactory func(cfg *ControllerConfig, arcCfg *rabbitmq.ArchitectureConfig, arc *rabbitmq.Architecture) *controller.Controller
+
 var log = logging.MustGetLogger("log")
 
+var controllerFactories = map[string]ControllerFactory{
+	"MFGQ1": CreateMFGQ1,
+	"MFGQ2": CreateMFGQ2,
+	"MFGQ3": CreateMFGQ3,
+	"MFGQ4": CreateMFGQ4,
+	"MFGQ5": CreateMFGQ5,
+	"MFRQ3": CreateMFRQ3,
+	"MFRQ4": CreateMFRQ4,
+	"MFRQ5": CreateMFRQ5,
+	"Q1S2":  CreateQ1S2,
+	"Q1S3":  CreateQ1S3,
+	"Q2S2":  CreateQ2S2,
+	"Q2S3":  CreateQ2S3,
+	"Q3S2":  CreateQ3S2,
+	"Q3S3":  CreateQ3S3,
+	"Q4S2":  CreateQ4S2,
+	"Q4S3":  CreateQ4S3,
+	"Q5S2":  CreateQ5S2,
+	"Q5S3":  CreateQ5S3,
+}
+
 func main() {
-	common.InitLogger("DEBUG")
-	_, err := common.InitConfig("/app/config.yaml")
+	var arcCfg = rabbitmq.LoadConfig("./architecture.yaml")
+	var arc = rabbitmq.CreateArchitecture(arcCfg)
+	var _, err = common.InitConfig("./common.yaml")
+	var controllersConfig = LoadConfig("./controllers.yaml")
 	if err != nil {
-		panic("Nos fuimos")
+		log.Fatal(err)
 	}
-	rabbit := rabbitmq.NewRabbit()
 
-	exG := rabbit.NewExchange(common.ExchangeNameGames, common.ExchangeFanout)
-	MFG_Q1 := rabbit.NewQueue("MFG_Q1")
-	MFG_Q1.Bind(exG, "")
-	log.Debug("Created map and filter exchange and binded MFG_Q1")
+	var wg sync.WaitGroup
 
-	exQ1_2 := rabbit.NewExchange("Q1_2", common.ExchangeDirect)
-	Q1_2 := rabbit.NewQueue("Q1_2")
-	Q1_2.Bind(exQ1_2, "1")
-	log.Debug("Created Q1 stage 2 exchange and binded Q1_2 with '1' ")
+	for _, controllerConfig := range controllersConfig.Controllers {
+		c, ok := controllerFactories[controllerConfig.Type]
+		if !ok {
+			log.Fatalf("Can't find controller for %s", controllerConfig.Type)
+		}
+		wg.Add(1)
 
-	exQ1_3 := rabbit.NewExchange("Q1_3", common.ExchangeDirect)
-	Q1_3 := rabbit.NewQueue("Q1_3")
-	Q1_3.Bind(exQ1_3, "1")
-	log.Debug("Created Q1 stage 3 exchange and binded Q1_3 with '1' ")
+		go func(cfg ControllerConfig) {
+			defer wg.Done()
+			c(&cfg, arcCfg, arc).Start()
+		}(controllerConfig)
 
-	exR_Q1 := rabbit.NewExchange("RESULT_Q1", common.ExchangeFanout)
-	R_Q1 := rabbit.NewQueue("RESULT_Q1")
-	R_Q1.Bind(exR_Q1, "")
-	log.Debug("Created Q1 Result exchange and binded ResultQ1")
-
-	workerController := common.Config.GetString("controller")
-
-	if workerController == "MAP_FILTER_GAMES_Q1" {
-		log.Debug("This is a MFG_Q1")
-		c := controller.NewController(
-			[]*rabbitmq.Queue{
-				MFG_Q1,
-			},
-			[]*rabbitmq.Exchange{
-				exQ1_2,
-			},
-			&controller.NodeProtocol{
-				PartitionAmount: 1,
-			},
-			func(jobId common.JobID) (controller.Handler, controller.EOFValidator, error) {
-				return &business.MapFilterGames{
-					Filter: nil,
-					Mapper: business.Q1Map,
-				}, controller.NewEOFChecker("MAP_FILTER_GAMES", 1), nil
-			},
-		)
-
-		go c.Start()
-	} else if workerController == "Q1_2" {
-		log.Debug("This is a Q1_2")
-		c := controller.NewController(
-			[]*rabbitmq.Queue{
-				Q1_2,
-			},
-			[]*rabbitmq.Exchange{
-				exQ1_3,
-			},
-			&controller.NodeProtocol{
-				PartitionAmount: 1,
-			},
-			func(jobId common.JobID) (controller.Handler, controller.EOFValidator, error) {
-				check := controller.NewEOFChecker("Q1_STAGE_2", 1)
-				h, err := business.NewQ1("./storage", jobId.String(), "stage_2")
-				if err != nil {
-					return nil, check, err
-				}
-				return h, check, nil
-			},
-		)
-
-		go c.Start()
-	} else if workerController == "Q1_3" {
-		log.Debug("This is a Q1_3")
-		c := controller.NewController(
-			[]*rabbitmq.Queue{
-				Q1_3,
-			},
-			[]*rabbitmq.Exchange{
-				exR_Q1,
-			},
-			&controller.NodeProtocol{
-				PartitionAmount: 1,
-			},
-			func(jobId common.JobID) (controller.Handler, controller.EOFValidator, error) {
-				check := controller.NewEOFChecker("Q1_STAGE_3", 1)
-				h, err := business.NewQ1("./storage", jobId.String(), "stage_3")
-				if err != nil {
-					return nil, check, err
-				}
-				return h, check, nil
-			},
-		)
-
-		go c.Start()
 	}
+
+	wg.Wait()
 }
